e0899879c53db6c13aecffd0a3ee6d9d7747b927
ThierryMts <32282642+ThierryMts@users.noreply.github.com>
2017-10-19 18:52:57 +0200

    Add files via upload

---
+++ test2.rb
@@ -0,0 +1,16 @@
+class BoardCase
+  #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide), ainsi que son numéro de case)
+   
+
+
+tab1 = [[1], [2], [3]]
+puts "---+---+---"
+tab2 = [[4], [5], [6]]
+puts "---+---+---"
+tab3 = [[7], [8], [9]]
+
+print "#{tab1} \n#{tab2} \n#{tab3}"
+
+  end
+
+  d
